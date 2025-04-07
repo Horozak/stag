8898e6ed4ebd606e4564b561667f5ed9dfc09624
kratomi1 <kratochvil3@email.cz>
2025-04-07 12:13:20 +0200

    novy kod

--- src/main/java/pro1/Main4.java
+++ src/main/java/pro1/Main4.java
@@ -1,5 +1,12 @@
 package pro1;
 
+import com.google.gson.Gson;
+import pro1.apiDataModel.Teacher;
+import pro1.apiDataModel.TeachersList;
+
+import java.util.Comparator;
+import java.util.List;
+
 public class Main4 {
 
     public static void main(String[] args) {
@@ -8,6 +15,15 @@ public class Main4 {
 
     public static void printShortestEmails(String department, int count)
     {
-        // TODO 4.1: Vypiš do konzole "count" nejkratších učitelských emailových adres
+        String json2 = Api.getTeachersByDepartment(department);
+        TeachersList teachears = new Gson().fromJson(json2, TeachersList.class);
+
+
+        teachears.items.stream().filter(t -> t.email != null)
+                .sorted(Comparator.comparing(t -> t.email.length()))
+                .limit(count).forEach(t -> System.out.println(t.email));
+
+        //Muzeme pouzit for
+
     }
 }
